9bb5c1b6752712d421741d59940e43872cc127ae
Yuri Kobyzev <yuri.kobyzev@mail.ru>
2023-06-10 18:16:41 +0300

    inference main program

--- runQuadro.py
+++ runQuadro.py
@@ -1,17 +1,41 @@
+import sys
+from pathlib import Path
+import random
+
 import numpy
 from Quadro import Quadro
+import pandas as pd
+
+random.seed(41)
+
+TEST_IMAGES_PATH, SAVE_PATH = sys.argv[1:]
+
 
 class QuadroCopter(Quadro):
     def __init__(self,conf_path: str):
        super().__init__(conf_path) 
        self.save_txt=True
+       self.src=TEST_IMAGES_PATH,
+       self.results=[]
 
     def save_labels(self,txtpath,cls,xywh,conf,txt_path,frame):
-       #line = (cls, *xywh, conf) if self.save_conf else (cls, *xywh)  # label format
-       line = (frame,cls,conf,*xywh)  # label format
-       with open(f'{txt_path}.txt', 'a') as f:
-           f.write(('%g ' * len(line)).rstrip() % line + '\n')
-   
+       xc = xywh[0]
+       yc = xywh[1]
+       w = xywh[2]
+       h = xywh[3]
+       image_id = txtpath
+
+       result = {
+          'image_id': image_id,
+          'xc': round(xc, 4),
+          'yc': round(yc, 4),
+          'w': round(w, 4),
+          'h': round(h, 4),
+          'label': 0,
+          'score': round(conf, 4)
+       }
+       self.results.append(result)
+
 
 def main():
     q=QuadroCopter('quadro.yaml')
@@ -20,7 +44,9 @@ def main():
     q.load_model()
     q.get_dataloader()
     q.run_inference()
+    test_df = pd.DataFrame(q.results, columns=['image_id', 'xc', 'yc', 'w', 'h', 'label', 'score'])
+    test_df.to_csv(SAVE_PATH, index=False)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
-    print('finish')
